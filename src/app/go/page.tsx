0268b8932bc8ec8a12a85198c78af88f9fd599c6
최홍석 <che0618@naver.com>
2024-10-22 09:36:20 +0900

    fix: suspense 추가

--- src/app/go/page.tsx
+++ src/app/go/page.tsx
@@ -2,7 +2,7 @@
 
 import { useSearchParams } from "next/navigation";
 import GoogleMap from "@/component/GoogleMap";
-import { useState } from "react";
+import { useState, Suspense } from "react";
 
 export default function MapPage() {
   const searchParams = useSearchParams();
@@ -15,21 +15,23 @@ export default function MapPage() {
   };
 
   return (
-    <div className="flex flex-col gap-y-4">
-      <h1>This page using google map</h1>
-      <h2>지도 언어 바꾸기</h2>
-      <span>현재언어 {language}</span>
+    <Suspense>
+      <div className="flex flex-col gap-y-4">
+        <h1>This page using google map</h1>
+        <h2>지도 언어 바꾸기</h2>
+        <span>현재언어 {language}</span>
 
-      <button type="button" onClick={() => handleLanguageChange("ja")}>
-        일어
-      </button>
-      <button type="button" onClick={() => handleLanguageChange("zh")}>
-        중어
-      </button>
-      <button type="button" onClick={() => handleLanguageChange("ko")}>
-        한국어
-      </button>
-      <GoogleMap language={language} />
-    </div>
+        <button type="button" onClick={() => handleLanguageChange("ja")}>
+          일어
+        </button>
+        <button type="button" onClick={() => handleLanguageChange("zh")}>
+          중어
+        </button>
+        <button type="button" onClick={() => handleLanguageChange("ko")}>
+          한국어
+        </button>
+        <GoogleMap language={language} />
+      </div>
+    </Suspense>
   );
 }
